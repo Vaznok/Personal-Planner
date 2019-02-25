b517bff8733ff1d44e2a7b4aca2be03ea1461710
Vitaliy Vlasenko <vlasenko.vitaliy.l@gmail.com>
2019-02-25 18:00:03 +0200

    first commit

---
+++ src/main/java/com/vlasenko/pptool/PpToolApplication.java
@@ -0,0 +1,13 @@
+package com.vlasenko.pptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpToolApplication.class, args);
+	}
+
+}
